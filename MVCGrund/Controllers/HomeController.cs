4fa4e86e5baf288b8ade4fe9823de2e7241f0bcc
David Nokto <david.nokto@gmail.com>
2022-06-30 11:54:19 +0200

    Add project files.

---
+++ MVCGrund/Controllers/HomeController.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace MVCGrund.Controllers
+{
+    public class HomeController : Controller
+    {
+
+        public HomeController()
+        {
+
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+
+        }
+
+
+
+    }
+}
